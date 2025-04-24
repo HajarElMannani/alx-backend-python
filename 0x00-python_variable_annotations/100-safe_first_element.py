3e51a4db742d6e171963b27fd9867d1d855e1975
HajarElMannani <laso125@hotmail.com>
2025-04-24 20:23:52 +0000

    Add documentation

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -5,6 +5,7 @@ from typing import Sequence, Any, Union
 
 # The types of the elements of the input are not know
 def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
+    '''Add annotation to the given function'''
     if lst:
         return lst[0]
     else:
