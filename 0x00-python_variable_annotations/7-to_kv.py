b380bca14c8f4913f2607095e001e3cd38f5fedd
HajarElMannani <laso125@hotmail.com>
2025-04-24 18:33:27 +0000

    Pycodestyle

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -3,7 +3,7 @@
 from typing import Union, Tuple
 
 
-def to_kv(k: str, v:Union[int, float]) -> Tuple[str, float]:
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     '''Function that returns a tuple of string k, and the square
     of the int/float v.'''
     return (k, v**2)
