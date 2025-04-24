78320ec8e6855c99b60bf1b60bd442f802157f87
HajarElMannani <laso125@hotmail.com>
2025-04-24 18:05:48 +0000

    First commit

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+'''floor function'''
+import math
+
+def floor(n: float) -> int:
+    '''Function which  takes a float n as argument and returns the
+    floor of the float'''
+    return math.floor(n)
---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+'''Function sum_list'''
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    '''Function that takes a list input_list of floats as argument
+    and returns their sum as a float'''
+    summ = 0.0;
+    for number in input_list:
+        summ = summ + number
+    return summ
