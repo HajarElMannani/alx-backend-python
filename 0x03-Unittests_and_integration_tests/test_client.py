116286877dffb3b03f688d4aa9618277c228542a
HajarElMannani <laso125@hotmail.com>
2025-05-18 16:18:07 +0000

    add test

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -23,3 +23,17 @@ class TestGithubOrgClient(unittest.TestCase):
         mock_get_json.assert_called_once_with(
             "https://api.github.com/orgs/{}".format(org_name)
         )
+
+    def test_public_repos_url():
+        '''test GithubOrgClient._public_repos_url'''
+        with patch(
+                "client.GithubOrgClient.org",
+                new_callable=PropertyMock,
+                ) as mock_org:
+            mock_org.return_value = {
+                'repos_url': 'https://api.github.com/users/google/repos',
+            }
+            self.assertEqual(
+                GithubOrgClient('googl')._public_repos-url,
+                'https://api.github.com/users/google/repos',
+            )
