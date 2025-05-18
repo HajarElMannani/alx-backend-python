3cb4774cb5500f2e45412a6ddd938c357e0d340d
HajarElMannani <laso125@hotmail.com>
2025-05-18 19:37:01 +0000

    Change test

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -3,7 +3,7 @@
 from parameterized import parameterized
 import client
 from client import GithubOrgClient
-from unittest.mock import patch
+from unittest.mock import patch, PropertyMock
 import unittest
 
 
@@ -40,9 +40,16 @@ class TestGithubOrgClient(unittest.TestCase):
     @patch('client.get_json')
     def test_public_repos(self, license, mock_get_json):
         '''test the method public_repos'''
+        test_payload = [
+            {'name': 'repo1', 'license': {'key': 'MIT'}}
+            {'name': 'repo2', 'license': {'key': 'AFL-3.0'}}
+        ]
+        mock_get_json.return_value = test_payload
         with patch.object(GithubOrgClient,
-                          'public_repos',
-                          return_value=["repo1"]) as mock_method:
-            result = GithubOrgClient().public_repos()
-            self.assertEqual(result, "repo1")
+                          'public_repos_url',
+                          new_callable=PropertyMock
+        ) as mock_method:
+            mock_method.return_value = {repos_url: 'https://api.github.com/users/abc/repos'}
+            result = GithubOrgClient('abc').public_repos()
+            self.assertEqual(result, ['repo1', 'repo2'])
         mock_get_json.assert_called_once()
